4d466902c153454ba8193a48ee6ee0959cfc0e57
Johanna Olivares Toro <johannaolivarestoro@MacBook-Air-de-Joha.local>
2024-05-24 19:59:13 -0400

    Modificación dqs

--- src/components/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer.jsx
@@ -1,21 +1,28 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 
+import cintasImage from '../assets/cintas.jpeg';
+import confetiImage from '../assets/confeti.jpeg';
+import varitasImage from '../assets/varitas.jpeg';
+
 const productDetails = {
   cintas1: {
     name: 'Ramo de cintas',
     description: 'Un hermoso ramo de cintas de colores para celebrar.',
-    price: '$30.000'
+    price: '$30.000',
+    image: cintasImage 
   },
   confeti1: {
     name: 'Confeti de colores',
     description: 'Confeti multicolor personalizado con tus colores favoritos.',
-    price: '$20.000'
+    price: '$20.000',
+    image: confetiImage 
   },
   varitas1: {
     name: 'Varitas de buenos deseos',
     description: 'Varitas mágicas hechas con amor para repartir buenos deseos.',
-    price: '$10.000'
+    price: '$10.000',
+    image: varitasImage 
   }
 };
 
@@ -28,6 +35,7 @@ const ItemDetailContainer = () => {
       {product ? (
         <>
           <h2>{product.name}</h2>
+          <img src={product.image} alt={product.name} /> {/* Agrega la imagen */}
           <p>{product.description}</p>
           <p>Precio: {product.price}</p>
         </>
